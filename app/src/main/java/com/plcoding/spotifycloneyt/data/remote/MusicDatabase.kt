784c35149aa9d70acb2a607a24b870ebd89b57f3
gulten27 <ggdogann00@gmail.com>
2023-03-21 10:29:14 +0300

    Dagger-Hilt Setup

--- app/src/main/java/com/plcoding/spotifycloneyt/data/remote/MusicDatabase.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/remote/MusicDatabase.kt
@@ -4,17 +4,16 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.plcoding.spotifycloneyt.data.entities.Song
 import com.plcoding.spotifycloneyt.other.Constants.SONG_COLLECTION
 import kotlinx.coroutines.tasks.await
-import java.lang.Exception
 
 class MusicDatabase {
 
     private val firestore = FirebaseFirestore.getInstance()
     private val songCollection = firestore.collection(SONG_COLLECTION)
 
-    suspend fun getAllSongs() : List<Song>{
+    suspend fun getAllSongs(): List<Song> {
         return try {
             songCollection.get().await().toObjects(Song::class.java)
-        }catch (e:Exception){
+        } catch(e: Exception) {
             emptyList()
         }
     }
