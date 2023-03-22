f879deaefc285497280734d09507eabba9863078
gulten27 <ggdogann00@gmail.com>
2023-03-23 00:44:04 +0300

    Preparing the Music Player

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/FirebaseMusicSource.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/FirebaseMusicSource.kt
@@ -18,7 +18,7 @@ class FirebaseMusicSource @Inject constructor(
     private val musicDatabase: MusicDatabase
 ){
 
-    private var songs = emptyList<MediaMetadataCompat>()
+    var songs = emptyList<MediaMetadataCompat>()
 
     suspend fun fetchMediaData() = withContext(Dispatchers.IO){
         state = State.STATE_INITIALIZING
