dcef6a1a55ac229072b3f8035413c2934f107e47
gulten27 <ggdogann00@gmail.com>
2023-03-25 00:17:41 +0300

    MainViewModel

--- app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
@@ -5,16 +5,22 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.plcoding.spotifycloneyt.R
+import com.plcoding.spotifycloneyt.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
-
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
 
     @Singleton
     @Provides
